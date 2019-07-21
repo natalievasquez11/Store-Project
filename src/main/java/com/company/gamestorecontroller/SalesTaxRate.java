b0b5a0955c5b93a842bca8eb25aa6d84727b2e38
Natalie Vasquez <natalievasquez11@gmail.com>
2019-07-21 18:51:40 -0500

    created salesTaxRate DTO

---
+++ src/main/java/com/company/gamestorecontroller/SalesTaxRate.java
@@ -0,0 +1,35 @@
+package com.company.gamestorecontroller;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name="sales_tax_rate")
+public class SalesTaxRate {
+    @NotNull
+    @Length(min=2, max=2)
+    private String state;
+    @NotNull
+    private Double rate;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+}
